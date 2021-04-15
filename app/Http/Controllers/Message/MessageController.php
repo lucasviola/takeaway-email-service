9c38c6404ae38efef3b1f495419bcffe61b22061
Lucas Viola <lucasmatzenbacher@gmail.com>
2021-04-15 18:14:12 -0300

    Adds simple MessageController and action for sending messages

---
+++ app/Http/Controllers/Message/MessageController.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace App\Http\Controllers\Message;
+
+use App\Http\Controllers\Controller;
+
+class MessageController extends Controller
+{
+    public function send() {
+        return 202;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Message')->group(function () {
+
+    Route::get('/message', 'MessageController@send');
+
 });

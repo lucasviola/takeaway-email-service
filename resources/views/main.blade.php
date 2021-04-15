cbb60f4e79a40c6dc889b244304e278791a8ee41
Lucas Viola <lucasmatzenbacher@gmail.com>
2021-04-15 16:15:23 -0300

    WIP - Fixes names in docker compose and views

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,20 +1,14 @@
 <html>
 <head>
     <link rel="stylesheet" href="/css/global.css" type="text/css">
-    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
 
-    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
-    <script src="/js/map.js"></script>
 
-    <title>My Travels</title>
 </head>
 
 <body class="container">
 
 <header>
-    <h1>My Travels</h1>
+    <h1>Takeway Email Exchange</h1>
 </header>
 
 <div class="main">
@@ -26,7 +20,6 @@
 </div>
 
 
-@yield('script')
 
 
 </body>
